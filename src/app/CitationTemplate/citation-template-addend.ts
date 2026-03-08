9a28138faae0a31c21921b9249ec7177a57e3fa9
Duncan Morais <duncanmorais@gmail.com>
2026-03-08 01:40:31 +0000

    remove junk

--- src/app/CitationTemplate/citation-template-addend.ts
+++ src/app/CitationTemplate/citation-template-addend.ts
@@ -1,23 +1,21 @@
-import { CitationTemplateVariable } from "./citation-template-variable";
-
 export class CitationTemplateAddend {
-    private displayValue: string;
-    private templateValue: string;
+  private displayValue: string;
+  private templateValue: string;
 
-    constructor(displayValue: string, templateValue: string) {
-        this.displayValue = displayValue;
-        this.templateValue = templateValue;
-    }
+  constructor(displayValue: string, templateValue: string) {
+    this.displayValue = displayValue;
+    this.templateValue = templateValue;
+  }
 
-    toString() {
-        return this.displayValue;
-    }
+  toString() {
+    return this.displayValue;
+  }
 
-    stringify() {
-        return this.templateValue;
-    }
+  stringify() {
+    return this.templateValue;
+  }
 
-    getVariables(): string[] {
-      throw 'Needs implementation'
-    }
+  getVariables(): string[] {
+    throw 'Needs implementation';
+  }
 }
